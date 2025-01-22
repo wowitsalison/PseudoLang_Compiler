63a2ed732414e6b7628e85219e13016e62996971
wowitsalison <aliatmra@gmail.com>
2025-01-21 20:26:05 -0600

    Populate symbol_table.cpp with comments

--- src/symbol_table.cpp
+++ src/symbol_table.cpp
@@ -1,51 +1,56 @@
 #include "symbol_table.h"
 
+// Constructor for SymbolTable class
 SymbolTable::SymbolTable() {
     enterScope();
 }
 
+// Add empty map to the scopes stack to enter a new scope
 void SymbolTable::enterScope() {
     scopes.push_back({});
 }
 
+// Remove top map from scopes stack to exit current scope
 void SymbolTable::exitScope() {
-    if (scopes.size() > 1) { 
-        scopes.pop_back();
+    if (scopes.size() > 1) { // Keeps it from removing the global scope
+        scopes.pop_back(); 
     }
 }
 
+// Declare new variable in current scope
 void SymbolTable::declareVariable(const std::string& name, VariableType type) {
-    if (!scopes.empty()) {
-        scopes.back()[name] = type;
+    if (!scopes.empty()) { // Make sure there is a scope to declare the variables in
+        scopes.back()[name] = type; // Add the variable to the current scope
     }
 }
 
+// Retrieve the type of a variable
 VariableType SymbolTable::getVariableType(const std::string& name) const {
-    for (auto it = scopes.rbegin(); it != scopes.rend(); ++it) {
-        auto found = it->find(name);
-        if (found != it->end()) {
-            return found->second;
+    for (auto it = scopes.rbegin(); it != scopes.rend(); ++it) { // Iterate through all scopes
+        auto found = it->find(name); 
+        if (found != it->end()) { 
+            return found->second; // Return the type of the variable
         }
     }
-    return VariableType::UNKNOWN;
+    return VariableType::UNKNOWN; // Return UNKNOWN if variable is not found
 }
 
+// Make sure variable is declared
 bool SymbolTable::isVariableDeclared(const std::string& name) const {
-    for (auto it = scopes.rbegin(); it != scopes.rend(); ++it) {
-        if (it->find(name) != it->end()) {
-            return true;
+    for (auto it = scopes.rbegin(); it != scopes.rend(); ++it) { // Iterate through all scopes
+        if (it->find(name) != it->end()) { 
+            return true; // Return true if variable is found
         }
     }
-    return false;
+    return false; // Return false if variable is not found
 }
 
+// Return all variables in the symbol table
 std::set<std::string> SymbolTable::getAllVariables() const {
-    std::set<std::string> allVariables;
-    // Iterate through all scopes from innermost to outermost
-    for (auto scopeIt = scopes.rbegin(); scopeIt != scopes.rend(); ++scopeIt) {
-        // Add all variables from current scope
+    std::set<std::string> allVariables; // Create a set to store all variables
+    for (auto scopeIt = scopes.rbegin(); scopeIt != scopes.rend(); ++scopeIt) { // Iterate through all scopes
         for (const auto& pair : *scopeIt) {
-            allVariables.insert(pair.first);
+            allVariables.insert(pair.first); // Add the variable to the set
         }
     }
     return allVariables;
